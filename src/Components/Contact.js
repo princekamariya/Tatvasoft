7da9dfb603ec073dd6bc8ed745de2fa41ebb4aba
Prins Kamariya <kamriyaprince@gmail.com>
2023-05-23 11:09:10 +0530

    Added contact us form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
+import { useNavigate } from "react-router-dom";
+
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 const Contact = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [msg, setMsg] = useState("");
+    const navigate = useNavigate();
+
+    const handleMsg = (e) => {
+        console.log(e.target.value);
+        setMsg(e.target.value);
+    };
+    const handleName = (e) => {
+        console.log(e.target.value);
+        setName(e.target.value);
+    };
+    const handleEmail = (e) => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Succesfully Submitted Your Msg");
+        console.log(`Name: ${name} Email: ${email} Msg: ${msg}`);
+        navigate("/");
+    };
     return (
         <div className="App">
             <header className="App-header">
-                <p>Contact.</p>
+                <Typography variant="h5" gutterBottom>
+                    Contact Us
+                </Typography>
+                <form action="/" onSubmit={handleSubmit}>
+                    <TextField
+                        required
+                        fullWidth
+                        id="outlined-basic"
+                        label="name"
+                        variant="outlined"
+                        margin="dense"
+                        onChange={handleName}
+                    />
+                    <TextField
+                        type="email"
+                        required
+                        fullWidth
+                        id="outlined-basic"
+                        label="email"
+                        variant="outlined"
+                        margin="dense"
+                        onChange={handleEmail}
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        id="outlined-basic"
+                        label="Message"
+                        variant="outlined"
+                        margin="dense"
+                        onChange={handleMsg}
+                        inputProps={{
+                            style: {
+                                height: "3em",
+                            },
+                        }}
+                    />
+                    <Button type="submit" variant="contained">
+                        Submit
+                    </Button>
+                </form>
             </header>
         </div>
     );
